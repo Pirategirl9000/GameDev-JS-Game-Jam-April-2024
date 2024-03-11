0a0e0676177907911dd0e7831666758406e48046
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-03-11 20:08:27 +0000

    Somewhat basic stuff for key handling and deaths

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const rect = canvas.getBoundingClientRect();
+let FPS = //-----------------
 
+document.addEventListener("keydown", (e) => {keyPress(e.key);});
+document.addEventListener("keyup", (e) => {keyRelease(e.key);});
 
+function update() {
+    if (alive == 0) {return;}
+    
+    ctx.clearRect(0,0,canvas.width, canvas.height);
 
+    ctx.beginPath();
+    //----------------
+    ctx.closePath();
+}
 
+function keyDown(key) {
+    if (alive == 0) {return;}
+}
+
+function keyRelease(key) {
+    if (alive == 0) {return;}
+}
 
 function collision(obj1, obj2) { //requires objects with x, y, width, height attributes
     if (
@@ -12,15 +30,7 @@ function collision(obj1, obj2) { //requires objects with x, y, width, height att
          (obj1.x + obj1.width >= obj2.x && obj1.x + obj1.width <= obj2.x + obj2.width)) &&
          ((obj1.y >= obj2.y && obj1.y <= obj2.y + obj2.height) ||
          (obj1.y + obj1.height >= obj2.y && obj1.y + obj1.height <= obj2.y + obj2.height)))
-        ) {
-        return true;
-      }
+        ) {return true;}
 }
 
-function update() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-
-    ctx.beginPath();
-    //----------------
-    ctx.closePath();
-}
+let alive = setInterval(() => {update();}, FPS * 1000);
